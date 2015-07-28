d83a3d7e421d13d13d634848036da69ddb28ed71
Nesulises7 <ne$tne$tor13652nlima@lieison.com>
2015-07-28 12:26:44 -0600

    merger

--- Inventario/application/views/login/index.php
+++ Inventario/application/views/login/index.php
@@ -8,7 +8,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-<<<<<<< HEAD
 <meta content="" name="Unitee"/>
 <meta content="" name="Lieson"/>
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
@@ -18,7 +17,6 @@
 <link href="<?php echo $route;?>css/bootstrap-rtl.min.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $route;?>css/uniform.default.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $route;?>css/bootstrap-switch-rtl.min.css" rel="stylesheet" type="text/css"/>
-=======
 <meta content="" name="description"/>
 <meta content="" name="author"/>
 
