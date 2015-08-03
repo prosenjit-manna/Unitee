cd351ecfba0ad53a9f8ff38d68a666bfcb441b1e
Nesulises7 <ne$tne$tor13652nlima@lieison.com>
2015-08-03 10:33:44 -0600

    header footer dashboard

--- Inventario/application/views/dashboard/footer.php
+++ Inventario/application/views/dashboard/footer.php
@@ -10,30 +10,30 @@
 	</div>
 </div>
 
-<script src="assert/plugins/jquery.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery.min.js" type="text/javascript"></script>
 <script src="assert/plugins/jquery-migrate.min.js" type="text/javascript"></script>
 
-<script src="assert/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
-<script src="assert/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assert/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
-<script src="assert/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="assert/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="assert/plugins/jquery.cokie.min.js" type="text/javascript"></script>
-<script src="assert/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
-<script src="assert/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
-<script src="assert/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
-<script src="assert/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
-<script src="assert/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
-<script src="assert/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
-<script src="assert/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
-<script src="assert/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
-<script src="assert/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
-<script src="assert/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
-<script src="assert/dashboard/js/metronic.js" type="text/javascript"></script>
-<script src="assert/dashboard/js/layout.js" type="text/javascript"></script>
-<script src="assert/dashboard/js/quick-sidebar.js" type="text/javascript"></script>
-<script src="assert/dashboard/js/demo.js" type="text/javascript"></script>
-<script src="assert/dashboard/js/index.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/dashboard/js/metronic.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/dashboard/js/layout.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/dashboard/js/quick-sidebar.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/dashboard/js/demo.js" type="text/javascript"></script>
+<script src="<?php echo $route;?>assert/dashboard/js/index.js" type="text/javascript"></script>
 
 <?php
     if(isset($javascript) && is_array($javascript)){
