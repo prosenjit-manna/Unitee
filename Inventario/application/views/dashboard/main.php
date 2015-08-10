e474924a7c634a407972b8dd9b95a41579212a13
geabenitez <geabenitez@lieison.com>
2015-08-10 16:23:19 -0600

    Ahora si furula

--- Inventario/application/views/dashboard/main.php
+++ Inventario/application/views/dashboard/main.php
@@ -8,9 +8,9 @@
 			<div class="page-bar">
 				<ul class="page-breadcrumb">
 					<li>
-						<i class="fa fa-home"></i>
+						<i class="icon-home"></i>
                                                 <a href="<?php echo site_url("Dashboard/index/"); ?>">Home</a>
-						<i class="fa fa-angle-right"></i>
+						<i class="icon-angle-right"></i>
 					</li>
 					<li>
 						<a href="<?php echo site_url("Dashboard/index/"); ?>">Dashboard</a>
