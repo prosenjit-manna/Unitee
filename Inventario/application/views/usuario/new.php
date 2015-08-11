972378d99b7fe1c4c9021190e370f8e44decd736
Nesulises7 <nlima@lieison.com>
2015-08-11 16:00:57 -0600

    Crear Usuario protlet

--- Inventario/application/views/usuario/new.php
+++ Inventario/application/views/usuario/new.php
@@ -30,7 +30,7 @@
 						<i class="icon-angle-right"></i>
 					</li>
 					<li>
-						<a href="">Crear Usuario</a>
+						<a href="">Nuevo Usuario</a>
 					</li>
 				</ul>
 				<div class="page-toolbar">
@@ -44,8 +44,45 @@
 			<!-- BEGIN PAGE HEADER-->
 			<!-- END PAGE HEADER-->
 			<!-- BEGIN PAGE CONTENT-->
-			<div class="">
-				
+			<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN Portlet PORTLET-->
+					<div class="portlet light">
+						<div class="portlet-title">
+							<div class="caption font-green-sharp">
+								<i class="icon-speech font-green-sharp"></i>
+								<span class="caption-subject"> Portlet</span>
+								<span class="caption-helper">more samples...</span>
+							</div>
+							<div class="actions">
+								<div class="btn-group">
+									<a class="btn btn-circle btn-default " href="#" data-toggle="dropdown">
+									<i class="fa fa-user"></i> Cancelar <i class="fa fa-angle-down"></i>
+									</a>
+								</div>
+								<a href="#" class="btn btn-circle red-sunglo ">
+								<i class="fa fa-plus"></i> Crear </a>
+								<a class="btn btn-circle btn-icon-only btn-default" href="#">
+								<i class="icon-cloud-upload"></i>
+								</a>
+								<a class="btn btn-circle btn-icon-only btn-default fullscreen" href="#">
+								</a>
+							</div>
+						</div>
+						<div class="portlet-body">
+							<div class="scroller" style="height:200px" data-rail-visible="1" data-rail-color="yellow" data-handle-color="#a1b2bd">
+								<h4>Heading Text</h4>
+								<p>
+									 Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.
+								</p>
+								<p>
+									 nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.
+								</p>
+							</div>
+						</div>
+					</div>
+					<!-- END Portlet PORTLET-->
+				</div>
 			</div>
 			<!-- END PAGE CONTENT-->
 		</div>
