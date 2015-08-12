03d9ef6f01c4048d792429188b09b610fe8aefc7
geabenitez <geabenitez@lieison.com>
2015-08-11 18:28:56 -0600

    Se cambio fa fa a icon en login

--- Inventario/application/views/login/index.php
+++ Inventario/application/views/login/index.php
@@ -50,14 +50,14 @@
 			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
 			<label class="control-label visible-ie8 visible-ie9">Usuario</label>
 			<div class="input-icon">
-				<i class="fa fa-user"></i>
+				<i class="icon-user"></i>
                                 <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Usuario" id="username" name="username"/>
 			</div>
 		</div>
 		<div class="form-group">
 			<label class="control-label visible-ie8 visible-ie9">Contraseña</label>
 			<div class="input-icon">
-				<i class="fa fa-lock"></i>
+				<i class="icon-lock"></i>
                                 <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Contraseña" id="password" name="password"/>
 			</div>
 		</div>
