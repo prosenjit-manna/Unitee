c191655717d9cbe731dc515c9f09f6b75c1a34dd
geabenitez <geabenitez@lieison.com>
2015-08-12 12:38:38 -0600

    Se creo Model User_Edit, se cambio el titulo de User_New y User_Edit

--- Inventario/application/models/user/User_edit.php
+++ Inventario/application/models/user/User_edit.php
@@ -2,44 +2,44 @@
 
 get_instance()->load->interfaces("Interface");
 
-class User_new extends CI_Model implements PInterface{
-    
+class User_edit extends CI_Model implements PInterface{
+
     public function __construct() {
         parent::__construct();
     }
-    
+
     public function _css() {
-        
+
     }
 
     public function _init() {
         $this->load->helper("form");
-        $this->load->view("usuario/new");
+        $this->load->view("usuario/edit");
     }
 
     public function _install() {
-        
+
     }
 
     public function _js() {
-        
+
     }
 
     public function _jsLoader() {
-        
+
     }
 
     public function _rols() {
-        
+
     }
 
     public function _title() {
-        return "Unitee | Nuevo Usuario";
-        
+        return "Unitee | Editar Usuario";
+
     }
 
     public function _update() {
-        
+
     }
 
 }
