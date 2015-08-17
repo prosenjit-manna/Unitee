167b6073a4c3100169778b21865c39df9ba83ace
geabenitez <geabenitez@lieison.com>
2015-08-17 15:18:22 -0600

    Formulario nuevo proveedor

--- Inventario/application/views/proveedor/proveedor_new.php
+++ Inventario/application/views/proveedor/proveedor_new.php
@@ -7,7 +7,7 @@
 		<div class="page-content">
 			<!-- INICIO TITULO DE LA PAGINA -->
 			<h3 class="page-title">
-			Unitee - Nuevo Proveedor 
+			Unitee - Nuevo Proveedor
 			</h3>
 			<!-- FINAL TITULO DE LA PAGINA -->
 
@@ -40,7 +40,7 @@
 						<div class="portlet-title">
 							<div class="caption font-green-sharp">
 								<i class="icon-speech font-green-sharp"></i>
-								<span class="caption-subject">Informaci贸n Usuario</span>
+								<span class="caption-subject">Nuevo Proveedor</span>
 							</div>
 						</div>
 			
@@ -79,23 +79,75 @@
 													<!--/span-->
 													<div class="col-md-6">
                                                                                                             <h3 lass="form-section">Direcci贸n</h3><br>
-														<label class="control-label col-md-2">Local</label>
-														<div class="form-group col-md-10">
-                                                                                                                    <input required="" type="text" id="" name="" class="form-control input-circle" placeholder="Numero de Telefono">
+														<label class="control-label col-md-3">Local</label>
+														<div class="form-group col-md-9">
+                                                                                                                    <input required="" type="text" id="" name="" class="form-control input-circle" placeholder="Nombre del Local">
 														</div>
-                                                                                                                <label class="control-label col-md-2">Direcci贸n 1</label>
-														<div class="form-group col-md-10">
-                                                                                                                    <input required="" type="text" id="" name="" class="form-control input-circle" placeholder="Numero de Telefono">
+                                                                                                                <label class="control-label col-md-3">Direcci贸n 1</label>
+														<div class="form-group col-md-9">
+                                                                                                                    <input required="" type="text" id="" name="" class="form-control input-circle" placeholder="Nombre de la Direccin 1">
 														</div>
-                                                                                                                <label class="control-label col-md-2">Direcci贸n 2</label>
-														<div class="form-group col-md-10">
-                                                                                                                    <input required="" type="text" id="" name="" class="form-control input-circle" placeholder="Numero de Telefono">
+                                                                                                                <label class="control-label col-md-3">Direcci贸n 2</label>
+														<div class="form-group col-md-9">
+                                                                                                                    <input required="" type="text" id="" name="" class="form-control input-circle" placeholder="Nombre de la Direccion 2">
 														</div>
+                                                        <label class="control-label col-md-3">Pais</label>
+                                                        <div class="form-group col-md-9">
+                                                            <select class="form-control input-circle">
+                                                                <option selected="selected">Elige un Pais</option>
+                                                                <option>El Salvador</option>
+                                                                <option>Honduras</option>
+                                                                <option>Guatemala</option>
+                                                            </select>
+                                                        </div>
+                                                        <label class="control-label col-md-3">Departamento</label>
+                                                        <div class="form-group col-md-9">
+                                                            <select class="form-control input-circle">
+                                                                <option selected="selected">Elige un Departamento</option>
+                                                                <option>San Salvador</option>
+                                                                <option>La Libertad</option>
+                                                                <option>Sonsonate</option>
+                                                                <option>Santa Ana</option>
+                                                                <option>San Vicente</option>
+                                                                <option>La Union</option>
+                                                                <option>San Miguel</option>
+                                                                <option>Morazan</option>
+                                                                <option>Caba馻s</option>
+                                                                <option>Ahuachapan</option>
+                                                                <option>Chalatenango</option>
+                                                            </select>
+                                                        </div>
+                                                        <label class="control-label col-md-3">Ciudad</label>
+                                                        <div class="form-group col-md-9">
+                                                            <select class="form-control input-circle">
+                                                                <option selected="selected">Elige una Ciudad</option>
+                                                                <option>San Salvador</option>
+                                                                <option>La Libertad</option>
+                                                                <option>Sonsonate</option>
+                                                                <option>Santa Ana</option>
+                                                                <option>San Vicente</option>
+                                                                <option>La Union</option>
+                                                                <option>San Miguel</option>
+                                                                <option>Morazan</option>
+                                                                <option>Caba馻s</option>
+                                                                <option>Ahuachapan</option>
+                                                                <option>Chalatenango</option>
+                                                            </select>
+                                                        </div>
 														
 													<!--/span-->
-												</div>
-												
-											</div>
+												    </div>
+                                                    <div class="col-md-12">
+                                                        <h3 lass="form-section">Descripci髇</h3><br>
+                                                    </div>
+                                                    <label class="control-label col-md-3">Descripci髇 de la Empresa</label>
+                                                    <div class="form-group col-md-9">
+                                                     <textarea rows="4" cols="50" class="form-control input-circle">
+                                                     </textarea>
+                                                    </div>
+
+
+                                                </div>
 											<div class="form-actions right">
                                                                                             <a href="<?php echo site_url("/0/"); ?>" class="btn default">Cancelar</a>
                                                                                             <button  id="send" name="send" disabled="disabled" type="submit" class="btn blue"><i class="fa fa-check"></i>Guardar</button>
