22df231cd916e60064b43f403b723a37fddd07c5
Nesulises7 <nlima@lieison.com>
2015-08-18 15:55:43 -0600

    request los name form new proveedor

--- Inventario/application/controllers/TheProvider.php
+++ Inventario/application/controllers/TheProvider.php
@@ -8,7 +8,18 @@ class TheProvider extends CI_Controller {
     }
     
     public function NewProvider(){
-        echo "HOLA MUNDO";
+        $empresa = $_REQUEST['txt_empresa'];
+        $contacto = $_REQUEST['txt_contacto'];
+        $telefono = $_REQUEST['txt_telefono'];
+        $celular = $_REQUEST['txt_celular'];
+        $fax = $_REQUEST['txt_fax'];
+        $correo = $_REQUEST['txt_correo'];
+        $local = $_REQUEST['txt_local'];
+        $direccion1 = $_REQUEST['txt_direccion1'];
+        $direccion2 = $_REQUEST['txt_direccion2'];
+        $pais = $_REQUEST['txt_pais'];
+        $depto = $_REQUEST['txt_dpto'];
+        $ciudad = $_REQUEST['txt_ciudad'];
     }
     
 }
