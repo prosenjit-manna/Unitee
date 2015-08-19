ddfd285896ff8d2d85ecc44e365121a29f7b2267
Nesulises7 <nlima@lieison.com>
2015-08-19 12:02:44 -0600

    Models & View Tabla ver proveedores

---
+++ Inventario/application/views/proveedor/proveedor_view.php
@@ -0,0 +1,170 @@
+
+
+<!-- INICIO CONTENIDO -->
+	<div class="page-content-wrapper">
+
+	<!-- INICIO CONTENIDO -->
+		<div class="page-content">
+			<!-- INICIO TITULO DE LA PAGINA -->
+			<h3 class="page-title">
+			Unitee - Ver Proveedor
+			</h3>
+			<!-- FINAL TITULO DE LA PAGINA -->
+
+			<!-- INICIO BREADCUMBS -->
+			<div class="page-bar">
+				<ul class="page-breadcrumb">
+					<li>
+						<i class="icon-home"></i>
+                                                <a href="<?php echo site_url("/0/"); ?>">Home</a>
+						<i class="icon-angle-right"></i>
+					</li>
+					<li>
+						<a href="#">Ver Proveedor</a>
+					</li>
+				</ul>
+				<div class="page-toolbar">
+                                    
+				</div>
+			</div>
+			<!-- FINAL BREADCUMBS -->
+
+			<!-- INICIO DASHBOARD STATS -->
+           	<div class="page-content-wrapper">
+           		<div class="row">
+					<div class="col-md-12">
+						<!-- BEGIN SAMPLE TABLE PORTLET-->
+						<div class="portlet">
+							<div class="portlet-title">
+								<div class="caption">
+									<i class="fa fa-bell-o"></i>Advance Table
+								</div>
+							</div>
+							<div class="portlet-body">
+								<div class="table-scrollable">
+									<table class="table table-striped table-bordered table-advance table-hover">
+									<thead>
+									<tr>
+										<th>
+											<p align="center">Codigo</p>
+										</th>
+										<th >
+											<p align="center">Empresa</p>
+										</th>
+										<th >
+											<p align="center">Departamento</p>
+										</th>
+										<th>
+											<p align="center">Nombre del Contacto</p>
+										</th>
+										<th>
+											<p align="center">Números de Telefono</p>
+										</th>
+										<th>
+											<p align="center">Descripción</p>
+										</th>
+										<th>
+											<p align="center">Operaciones</p>
+										</th>
+									</tr>
+									</thead>
+									<tbody>	
+									<tr>
+										<td class="highlight">
+											<p align="center">123</p>
+										</td>
+										<td  align="center">
+											 Google Inc.
+										</td>
+										<td>
+											<p align="center">La Libertad</p>
+										</td>
+										<td>
+											 <p align="center">Nestor U. Lima</p>
+										</td>
+										<td>
+											<p align="center">22222-0000 / 77778822</p>
+										</td>
+										<td>
+											 <p align="center">
+											 	Desarrollo de Software y Aplicaciones moviles Nativas 
+											 </p>
+										</td>
+										<td>
+											<p align="center">
+												<a href="#"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a href="#"><i class="icon-pencil" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a href="#"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
+											</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="highlight">
+											<p align="center">123</p>
+										</td>
+										<td  align="center">
+											 Google Inc.
+										</td>
+										<td>
+											<p align="center">La Libertad</p>
+										</td>
+										<td>
+											 <p align="center">Nestor U. Lima</p>
+										</td>
+										<td>
+											<p align="center">22222-0000 / 77778822</p>
+										</td>
+										<td>
+											 <p align="center">
+											 	Desarrollo de Software y Aplicaciones moviles Nativas 
+											 </p>
+										</td>
+										<td>
+											<p align="center">
+												<a href="#"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a href="#"><i class="icon-pencil" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a href="#"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
+											</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="highlight">
+											<p align="center">123</p>
+										</td>
+										<td  align="center">
+											 Google Inc.
+										</td>
+										<td>
+											<p align="center">La Libertad</p>
+										</td>
+										<td>
+											 <p align="center">Nestor U. Lima</p>
+										</td>
+										<td>
+											<p align="center">22222-0000 / 77778822</p>
+										</td>
+										<td>
+											 <p align="center">
+											 	Desarrollo de Software y Aplicaciones moviles Nativas 
+											 </p>
+										</td>
+										<td>
+											<p align="center">
+												<a href="#"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a href="#"><i class="icon-pencil" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a href="#"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
+											</p>
+										</td>
+									</tr>
+									</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<!-- END SAMPLE TABLE PORTLET-->
+					</div>
+				</div>	
+			</div>
+		<!-- FINAL ESTILOS DE LA BARRA -->
+
+	</div>
