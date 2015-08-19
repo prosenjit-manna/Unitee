5b3b3d30d8a842d79b00ceded71fae050a1c504d
Nesulises7 <nlima@lieison.com>
2015-08-19 14:52:18 -0600

    Modals ver proveedor

--- Inventario/application/views/proveedor/proveedor_view.php
+++ Inventario/application/views/proveedor/proveedor_view.php
@@ -92,9 +92,9 @@
 										</td>
 										<td>
 											<p align="center">
-												<a class="" data-toggle="modal" href="#responsive"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a class="" data-toggle="modal" href="#responsive_view"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
 												<a href="#"><i class="icon-pencil" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
-												<a href="#"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
+												<a class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
 											</p>
 										</td>
 									</tr>
@@ -121,9 +121,9 @@
 										</td>
 										<td>
 											<p align="center">
-												<a class="" data-toggle="modal" href="#responsive2"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a class="" data-toggle="modal" href="#responsive_view"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
 												<a href="#"><i class="icon-pencil" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
-												<a href="#"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
+												<a class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
 											</p>
 										</td>
 									</tr>
@@ -150,56 +150,134 @@
 										</td>
 										<td>
 											<p align="center">
-												<a class="" data-toggle="modal" href="#responsive"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
+												<a class="" data-toggle="modal" href="#responsive_view"><i class="icon-eye-open" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
 												<a href="#"><i class="icon-pencil" style="font-size: 20px;"></i></i></a>&nbsp;&nbsp;
-												<a href="#"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
+												<a class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></i></a>
 											</p>
 										</td>
 									</tr>
 									</tbody>
 									</table>
-									<div id="responsive" class="modal fade" tabindex="-1" aria-hidden="true">
-										<div class="modal-dialog">
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-													<h4 class="modal-title">Responsive & Scrollable</h4>
-												</div>
+									<div class="modal fade bs-modal-lg" id="responsive_view" tabindex="-1" role="dialog" aria-hidden="true">
+								<div class="modal-dialog modal-lg">
+									<div class="modal-content">
+										<div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+											<h4 class="modal-title">Información de Proveedor</h4>
+										</div>
 												<div class="modal-body">
 													<div class="scroller" style="height:300px" data-always-visible="1" data-rail-visible1="1">
 														<div class="row">
-															<div class="col-md-6">
-																<h4>Some Input</h4>
+															<div class="portlet light">
+																<div class="portlet-body">
+																	<div class="portlet-body form">
+																		<div class="form-body">
+																			<div class="row">
+																										<div class="col-md-6">
+																											<h3 lass="form-section">Información deContacto</h3><br>
+																										   <label class="control-label col-md-2">Empresa</label>
+																											<div class="form-group col-md-10">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_empresa" class="form-control input-circle" placeholder="Nombre de la Empresa">
+																											</div>
+																											<label class="control-label col-md-2">Contacto</label>
+																											<div class="form-group col-md-10">
+													                                                                                                                     <input disabled="disabled" required="" type="text" id="" name="txt_contacto" class="form-control input-circle" placeholder="Nombre de Contacto">
+																											</div>
+																											<label class="control-label col-md-2">Teléfono</label>
+																											<div class="form-group col-md-10">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_telefono" class="form-control input-circle" placeholder="Numero de Telefono">
+																											</div>
+													                                                                                                                <label class="control-label col-md-2">Celular</label>
+																											<div class="form-group col-md-10">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_celular" class="form-control input-circle" placeholder="Numero de Celular">
+																											</div>
+													                                                                                                                <label class="control-label col-md-2">Fax</label>
+																											<div class="form-group col-md-10">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_fax" class="form-control input-circle" placeholder="Numero de FAX">
+																											</div>
+													                                                                                                                <label class="control-label col-md-2">Correo</label>
+																											<div class="form-group col-md-10">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_correo" class="form-control input-circle" placeholder="Correo Electronico">
+																											</div>
+													                                                                                                                
+																										</div>
+																										<!--/span-->
+																										<div class="col-md-6">
+													                                                                                                            <h3 lass="form-section">Dirección</h3><br>
+																											<label class="control-label col-md-3">Local</label>
+																											<div class="form-group col-md-9">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_local" class="form-control input-circle" placeholder="Nombre del Local">
+																											</div>
+													                                                                                                                <label class="control-label col-md-3">Dirección 1</label>
+																											<div class="form-group col-md-9">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_direccion1" class="form-control input-circle" placeholder="Nombre de la Direccion 1">
+																											</div>
+													                                                                                                                <label class="control-label col-md-3">Dirección 2</label>
+																											<div class="form-group col-md-9">
+													                                                                                                                    <input disabled="disabled" required="" type="text" id="" name="txt_direccion2" class="form-control input-circle" placeholder="Nombre de la Direccion 2">
+																											</div>
+													                                                        <label class="control-label col-md-3">Pais</label>
+													                                                        <div class="form-group col-md-9">
+													                                                            <select id="select_country" disabled="disabled" name="txt_pais" class="form-control input-circle">
+													                                                               
+													                                                            </select>
+													                                                        </div>
+													                                                        <label class="control-label col-md-3">Departamento</label>
+													                                                        <div class="form-group col-md-9">
+													                                                            <select disabled="disabled" id="select_depto" name="txt_depto" class="form-control input-circle">
+													                                                               
+													                                                            </select>
+													                                                        </div>
+													                                                        <label class="control-label col-md-3">Ciudad</label>
+													                                                        <div class="form-group col-md-9">
+													                                                            <select id="select_city" disabled="disabled" class="form-control input-circle" name="txt_ciudad">
+													                                                                
+													                                                            </select>
+													                                                        </div>
+																											
+																										<!--/span-->
+																									    </div>
+													                                                    <div class="col-md-12">
+													                                                        <h3 lass="form-section">Descripci&oacute;n</h3><br>
+													                                                    </div>
+													                                                    <label class="control-label col-md-3">Descripci&oacute;n de la Empresa</label>
+													                                                    <div class="form-group col-md-9">
+													                                                        <textarea disabled="disabled" name="txt_descripcion" rows="4" cols="50" class="form-control input-circle" placeholder="Descripcion de la Empresa">
+													                                                     </textarea>
+													                                                    </div>
+													                      	</div>
+																		</div>
+																	</div>
+																</div>
 															</div>
 														</div>
 													</div>
 												</div>
 												<div class="modal-footer">
-													<button type="button" data-dismiss="modal" class="btn default">Close</button>
-													<button type="button" class="btn green">Save changes</button>
+													<button type="button" data-dismiss="modal" class="btn default">Cerrar</button>
 												</div>
 											</div>
 										</div>
 									</div>
-									<div id="responsive2" class="modal fade" tabindex="-1" aria-hidden="true">
+									<div id="responsive_delete" class="modal fade" tabindex="-1" aria-hidden="true">
 										<div class="modal-dialog">
 											<div class="modal-content">
 												<div class="modal-header">
 													<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-													<h4 class="modal-title">Responsive & Scrollable</h4>
+													<h4 class="modal-title">Eliminar Proveedor</h4>
 												</div>
 												<div class="modal-body">
-													<div class="scroller" style="height:300px" data-always-visible="1" data-rail-visible1="1">
+													<div class="scroller" style="height:30px" data-always-visible="1" data-rail-visible1="1">
 														<div class="row">
-															<div class="col-md-6">
-																<h4>Some Inputakhdsadjksahdjksahdsah</h4>
+															<div class="col-md-12">
+																<h4>¿Deseas eliminar a $name_proveedor de tu lista de proveedores?</h4>
 															</div>
 														</div>
 													</div>
 												</div>
 												<div class="modal-footer">
-													<button type="button" data-dismiss="modal" class="btn default">Close</button>
-													<button type="button" class="btn green">Save changes</button>
+													<button type="button" data-dismiss="modal" class="btn default">Cancelar</button>
+													<button type="button" class="btn green">Eliminar</button>
 												</div>
 											</div>
 										</div>
