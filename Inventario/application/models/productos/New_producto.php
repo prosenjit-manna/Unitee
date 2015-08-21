e6443b89f2faac92f1fe05e88b04d2ca773cfd82
Nesulises7 <nlima@lieison.com>
2015-08-21 10:39:42 -0600

    Crear Productos

---
+++ Inventario/application/models/productos/New_producto.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+   @@author: Lieison S.A de C.V
+   @@version: 1.2
+ * @@update : lieison.com/unitee/update
+ * @@type: plugin
+ * @@name: nuevo usuario
+ * @@parent: user
+ * @@description : modulo en el cual se encarga de crear un nuevo usuario 
+ * @@id : _provider_001
+ */
+
+
+get_instance()->load->interfaces("Interface");
+
+class New_producto extends CI_Model implements PInterface{
+    
+    
+    protected $model   = "new_producto";
+
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function _css() {
+        //ACA IRAN TODOS LOS CSS 
+    }
+
+    public function _init() {
+        $this->load->helper("form");
+        $this->load->view("producto/producto_new");
+    }
+
+    public function _install() {
+        //INSTALACION DEL MODULO 
+    }
+
+    public function _js() {
+        //ACA IRAN TODOS LOS JAVASCRIPT
+    }
+
+    public function _jsLoader() {
+
+    }
+
+    public function _rols() {
+       
+    }
+
+    public function _title() {
+        return "Unitee | Nuevo producto";
+        
+    }
+
+    public function _update() {
+        //ACTUALIZACION DEL MODULO UPDATE
+    }
+
+
+    public function _unistall() {
+        //DESISTALACION 
+    }
+}
+   
