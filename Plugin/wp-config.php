7474fcb0ab46579aeba950defdda3378371ffdb6
rolignu2 <rolignu90@gmail.com>
2015-09-07 20:20:31 -0600

    Rolando->plugin bug fixed

--- Plugin/wp-config.php
+++ Plugin/wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'unitee_plugin');
+define('DB_NAME', 'wp_unitee');
 
 /** MySQL database username */
 define('DB_USER', 'root');
