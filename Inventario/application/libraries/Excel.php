f088a54c23d269a521e5493ffdd2b49b58731c67
rolignu2 <rolignu90@gmail.com>
2015-09-14 15:23:51 -0600

    Rolando->Libreria EXCEL

--- Inventario/application/libraries/Excel.php
+++ Inventario/application/libraries/Excel.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @version 1.0
+ * @author  Rolando Arriaza 
+ * @todo Libreria excel para codeigniter
+ * @depends De la libreria phpExcel.php
+ * @name Excel
+ * 
+ * **/
+
 get_instance()->load->complement("EXCEL/PHPExcel");
 
 class Excel extends PHPExcel {
@@ -9,6 +18,12 @@ class Excel extends PHPExcel {
     }
     
     public function __construct() {
+        
+      /***
+        * ESTA LIBRERIA DEPENDE DE PHPEXCEL.PHP
+        *  ESTA DENTRO DE COMPLEMENT/EXCEL/..
+        * ***/
+        
         parent::__construct();
     }
     
