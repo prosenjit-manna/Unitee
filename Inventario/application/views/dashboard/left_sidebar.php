fabdf4679096e01b4b5c047387700a8be4dd8e0c
rolignu2 <rolignu90@gmail.com>
2015-09-17 09:57:23 -0600

    Rolando->Se agregaron las metas para el cargado del  sidebar ...

--- Inventario/application/views/dashboard/left_sidebar.php
+++ Inventario/application/views/dashboard/left_sidebar.php
@@ -4,16 +4,27 @@
 <!-- BEGIN CONTAINER -->
 <!-- BEGIN SIDEBAR -->
 <div class="page-sidebar-wrapper">
-		<div class="page-sidebar navbar-collapse collapse">
+		<div name="sidebar_" id="sidebar_" class="page-sidebar navbar-collapse collapse">
                     <!-- SIBEDAR INIT -->
-                    <ul name="sidebar_" id="sidebar_" class="page-sidebar-menu" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
-                        <!-- SIDEBAR SYSTEM COMO TAREA ASINCRONA O PROCESO DE SEGUNDO PLANO -->
-                        <li>
-                            <br><br>
-                            <a >Cargando ...</a>
-                        </li> 
-                          		
-                    </ul>
+                    <!-- SIDEBAR ASINC TASK-->
+                    <?php
+                        for($i=0; $i < 12; $i++):
+                           echo "<br>";
+                        endfor;
+                    ?>
+                    <div align="center">
+                        <?php
+                        
+                          $c_sidebar = &get_instance();
+                          $c_sidebar->load->library("metadata");
+                          $c_name       = $c_sidebar->metadata->GetMeta("sidebar_name");
+                          $c_logo       = $c_sidebar->metadata->GetMeta("sidebar_logo");
+                        ?>
+                        <img src="<?php echo site_url() . $c_logo->value; ?>" width="150" height="40" />
+                        <?php 
+                          echo $c_name->value;
+                        ?>
+                    </div>
                     <!-- END SIDEBAR -->
 		</div>
 	</div>
@@ -26,7 +37,9 @@
             var tasking = new jtask();
             tasking.url = "<?php echo site_url("/sidebar/"); ?>";
             tasking.success_callback(function(result){
-                 $("#sidebar_").html(result);
+                   var div = '<ul  class="page-sidebar-menu" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">';
+                   var sdiv = '</ul>';
+                  $("#sidebar_").html(div  + result + sdiv);
             });
             tasking.do_task();
         };
