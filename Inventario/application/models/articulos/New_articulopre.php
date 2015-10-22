3b27e91d6612065bc942baecaf85cbbc0b53a37e
nlima@lieison.com <ne$tor13652>
2015-10-22 11:41:54 -0600

    Nestor -> Se creo el model y el view de new articulo pre fabricado

---
+++ Inventario/application/models/articulos/New_articulopre.php
@@ -0,0 +1,126 @@
+<?php
+/**
+  @@author: Lieison S.A de C.V
+  @@version: 1.2
+ * @@update : lieison.com/unitee/update
+ * @@type: plugin
+ * @@name: nuevo usuario
+ * @@parent: user
+ * @@description : modulo en el cual se encarga de crear un nuevo usuario 
+ * @@id : _producto_001
+ */
+get_instance()->load->interfaces("Interface");
+get_instance()->load->interfaces("PluginConfig");
+
+class New_articulopre extends CI_Model implements PInterface {
+
+    use PluginConfig;
+
+    protected $model        = "new_articulopre";
+    
+    
+    var $route              = null;
+    var $path               = "";
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->database();
+
+        $this->_config = array(
+            "version"               => 1.0,
+            "author"                => "Lieison S.A de C.V",
+            "type"                  => "view",
+            "name"                  => "Ver clientes",
+            "description"           => "Modulo para crear clientes",
+            "id_model"              => "003",
+            "id_update"             => "005",
+            "update"                => "",
+            "license"               => "",
+            "controller"            => "",
+            "view"                  => "articulos/articulos_newpre"
+        );
+
+        $this->load->library("base_url");
+        $this->route = base_url();
+    }
+
+    public function _css() {
+        return array(
+            $this->route . "assert/plugins/select2/select2.css",
+            $this->route . "assert/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.css",
+            $this->route . "assert/perfil/css/profile.css",
+            $this->route . "assert/perfil/css/profile-old.css",
+            $this->route . "assert/perfil/css/bootstrap-fileinput.css"
+        );
+
+    }
+
+    public function _init() {
+        $this->load->view("articulos/articulo_newpre" , array(
+              "clients" => $clients
+        ));
+    }
+
+    public function _install() {
+        //INSTALACION DEL MODULO 
+    }
+
+    public function _js() {
+       return array(
+            $this->route . "assert/plugins/select2/select2.min.js",
+            $this->route . "assert/plugins/datatables/media/js/jquery.dataTables.min.js",
+            $this->route . "assert/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js",
+            $this->route . "assert/plugins/table-editable.js",
+            $this->route . "assert/perfil/js/profile.js",
+           $this->route . "assert/perfil/js/bootstrap-fileinput.js"
+        );
+    }
+
+    public function _jsLoader() {
+         return array("TableEditable.init();","table_loader();");
+    }
+
+    public function _rols() {
+        $this->load->model("system/permission_engine");
+        $data = $this->permission_engine->_get(
+                $this->model, MODEL, INT
+        );
+        return $data;
+    }
+
+    public function _title() {
+        return "Unitee | Nuevo Articulo";
+    }
+
+    public function _update() {
+        //ACTUALIZACION DEL MODULO UPDATE
+    }
+
+    public function _unistall() {
+        //DESISTALACION 
+    }
+
+    public function _Getconfig() {
+        return $this->_config;
+    }
+
+    public function _widgets() {
+        
+    }
+
+    public function _operations() {
+        $this->load->model("system/permission_engine", 'engine');
+        $data = $this->engine->GetOperationRoles($this->model);
+        return $data;
+    }
+
+    public function _Dashboard() {
+        
+    }
+
+    public function _JSdashboard() {
+        
+    }
+    
+}
---
+++ Inventario/application/views/articulos/articulo_newpre.php
@@ -0,0 +1,259 @@
+<div class="page-content-wrapper">
+    <!-- INICIO CONTENIDO -->
+    <div class="page-content">
+        <?php
+        $response = isset($_REQUEST['s']) ? $_REQUEST['s'] : -1;
+        if ($response != -1) {
+            switch ($response) {
+                case TRUE:
+                    echo '<div class="alert alert-block alert-success fade in">
+                       <button type="button" class="close icon-close" data-dismiss="alert" aria-hidden="true"></button><p>Articulo creado con exito</p>
+                                          </div>';
+                    break;
+                case FALSE:
+                    echo ' <div class="alert alert-block alert-danger fade in">
+                              <button type="button" class="close icon-close" data-dismiss="alert" aria-hidden="true"> </button><p>No se pudo crear el articulo,  favor intentar de nuevo.</p>
+                                        </div>';
+                    break;
+            }
+        }
+        ?>
+        <h3 class="page-title">
+            Unitee - Nuevo Articulo Prefabricado
+        </h3>
+        <!-- FINAL TITULO DE LA PAGINA -->
+        <!-- INICIO BREADCUMBS -->
+        <div class="page-bar">
+            <ul class="page-breadcrumb">
+                <li>
+                    <i class="icon-home"></i>
+                    <a href="<?php echo site_url("/0/"); ?>">Home</a>
+                    <i class="icon-angle-right"></i>
+                </li>
+                <li>
+                    <a href="#">Nuevo Articulo Prefabricado</a>
+                </li>
+            </ul>
+        </div>
+        <!-- FINAL BREADCUMBS -->
+        <div class="page-content-wrapper scroller" style="height:430px" data-rail-visible="1" data-rail-visible="1" data-rail-color="white" data-handle-color="black">
+            <!-- INICIO Portlet PORTLET-->
+            <div class="portlet">
+                <div class="portlet light">
+                    <div class="portlet-title">
+                        <div class="caption font-green-sharp">
+                            <i class="icon-speech font-green-sharp"></i>
+                            <span class="caption-subject bold uppercase">Nuevo Articulo</span>
+                        </div>
+                    </div>
+                    <div class="portlet-body">
+                        <!-- INICIO FORM-->
+                        <h5 lass="form-section">Los campos con * son Requeridos</h5>
+                        <div class="col-md-12">
+                            <h3 lass="form-section">General</h3>
+                            <div class="col-md-6">
+                                <br><label class="control-label col-md-4">* Nombre</label>
+                                <div class="form-group col-md-8">
+                                    <input autocomplete="off"  required="required" type="text" id="txt1" name="txt_nombre" class="form-control input-circle" placeholder="Nombre del producto">
+                                </div>
+                               <div style="padding-top: 88px;">
+                                    <label class="control-label col-md-4">Descripci&oacute;n</label>
+                                   <div class="form-group col-md-8">
+                                       <textarea name="txt_desc" class="form-control input-circle" rows="4" placeholder="Descripción del producto"></textarea>
+                                   </div>
+                               </div>
+                                    <label class="control-label col-md-4">Ver en tienda</label>
+                                   <div class="form-group col-md-8">
+                                       <input type="checkbox" class="make-switch" data-on-text="SI" data-off-text="No" data-on-color="primary" data-off-color="danger">
+                                   </div>
+                            </div>
+                            <div class="col-md-6"><br>
+                                    <label class="control-label col-md-4">* Productos</label>
+                                    <div class="form-group col-md-8">
+                                         <select onchange="select_color(this.value);" required="required" id="select_nombre" name="txt_pombre" class="form-control input-circle">                     
+                                                    <option value="-1">Seleccione el producto</option>
+                                                    <option value="">Camissa Blanca Polo</option>
+                                                    <option value="">Camiseta Negra</option>
+                                        </select>
+                                        <span class="help-block" style="font-size:8pt;">
+                                            Hay 225 camisas de este tipo su rango de precio es desde 
+                                            $2.25 hasta $5.25
+                                        </span>
+                                    </div>
+                                <label clas="control-label col-md-3" for="">* Seleccione las tallas</label><br>
+                                <div class="col-md-offset-1">
+                                    <label clas="control-label col-md-4" for="">
+                                        <input type="checkbox" onchange="_check();"  id="_talla_all">Marcar todas
+                                    </label>
+                                </div><br>
+                                <div id="talla_check" class="col-md-12">
+                                    <div class="col-md-2"><label clas="control-label">XS<input type="checkbox" name="checkboxx" id="checkboxx"></label></div>
+                                    <div class="col-md-2"><label clas="control-label">S<input type="checkbox"  name="checkbox" id="checkbox"></label></div>
+                                    <div class="col-md-2"><label clas="control-label">M<input type="checkbox"  name="checkbox" id="checkbox"></label></div>
+                                    <div class="col-md-2"><label clas="control-label">L<input type="checkbox"  name="checkbox" id="checkbox"></label></div>
+                                    <div class="col-md-2"><label clas="control-label">G<input type="checkbox"  name="checkbox" id="checkbox"></label></div>
+                                    <div class="col-md-2"><label clas="control-label">XL<input type="checkbox"  name="checkbox" id="checkbox"></label></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h3 lass="form-section">Productos</h3><br>
+                            <table class="table table-striped table-hover table-bordered" id="products_table">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                <p align="center">Tallas</p>
+                                </th>
+                                <th >
+                                <p align="center">Precio</p>
+                                </th>
+                                <th>
+                                <p align="center">Operaciones</p>
+                                </th>
+                                </tr>
+                                </thead>
+                                <tbody id="table_prod">
+                                    <tr>
+                                        <td align="center">XL</td>
+                                        <td align="center">
+                                            <input autocomplete="off"  required="required" type="text" id="txt1" name="txt_nombre" class="form-control input-circle" style="width: 200px;" placeholder="Escribe el preio">
+                                        </td>
+                                        <td align="center">
+                                            <a title="editar producto" href="#"><i class="icon-pencil" style="font-size: 20px;"></i></a>
+                                            <a title="eliminar producto " class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center">XS</td>
+                                        <td align="center">
+                                            <input autocomplete="off"  required="required" type="text" id="txt1" name="txt_nombre" class="form-control input-circle" style="width: 200px;" placeholder="Escribe el preio">
+                                        </td>
+                                        <td align="center">
+                                            <a title="editar producto" href="#"><i class="icon-pencil" style="font-size: 20px;"></i></a>
+                                            <a title="eliminar producto " class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center">L</td>
+                                        <td align="center">
+                                            <input autocomplete="off"  required="required" type="text" id="txt1" name="txt_nombre" class="form-control input-circle" style="width: 200px;" placeholder="Escribe el preio">
+                                        </td>
+                                        <td align="center">
+                                            <a title="editar producto" href="#"><i class="icon-pencil" style="font-size: 20px;"></i></a>
+                                            <a title="eliminar producto " class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td align="center">M</td>
+                                        <td align="center">
+                                            <input autocomplete="off"  required="required" type="text" id="txt1" name="txt_nombre" class="form-control input-circle" style="width: 200px;" placeholder="Escribe el preio">
+                                        </td>
+                                        <td align="center">
+                                            <a title="editar producto" href="#"><i class="icon-pencil" style="font-size: 20px;"></i></a>
+                                            <a title="eliminar producto " class="" data-toggle="modal" href="#responsive_delete"><i class="icon-trash" style="font-size: 20px;"></i></a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div><br>
+                    <!--/span-->
+                    <div class="col-md-12">
+                        <div class="form-actions col-md-offset-9">
+                            <a href="<?php echo site_url("/0/"); ?>" class="btn default">Cancelar</a>
+                            <button disabled="disabled" id="send" name="send"  type="submit" class="btn blue"><i class="fa fa-check"></i>Guardar</button>
+                        </div>
+                    </div>
+                    <!--Modals-->
+                    <div id="responsive_delete" class="modal fade" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                    <h4 class="modal-title">Eliminar Producto</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="scroller" style="height:30px" data-always-visible="1" data-rail-visible1="1">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <h4>¿Deseas eliminar este producto de tu lista?</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" data-dismiss="modal" class="btn default">Cancelar</button>
+                                    <button type="button" data-dismiss="modal" onclick="delete_product();" class="btn green">Eliminar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Modals-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FINAL Portlet PORTLET-->
+</div>
+</div>
+<script>
+  var _check = function(){
+     var prop = $("#_talla_all").prop("checked");
+     if(prop){
+         $("#talla_check")
+                .find("checkbox")
+                .prop("checked" , true);
+        $("#talla_check")
+                .find("span")
+                .addClass("checked");
+    }
+    else{
+        $("#talla_check")
+                .find("checkbox")
+                .prop("checked" , false);
+        $("#talla_check")
+                .find("span")
+                .removeClass()("checked");
+    }
+  };
+
+   var table_loader = function () {
+
+        var table = $('#products_table');
+
+        var oTable = table.dataTable({
+            "lengthMenu": [
+                [5, 15, 30, -1],
+                [5, 10, 30, "Todos"]
+            ],
+            "pageLength": 5,
+            "language": {
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                },
+                "emptyTable": "No data available in table",
+                "info": "Mostrando _START_ a _END_ en total de _TOTAL_ tallas",
+                "infoEmpty": "No se ha encontrado tallas ...",
+                "infoFiltered": "(filtered1 from _MAX_ total records)",
+                "lengthMenu": "Mostar _MENU_ Productos",
+                "search": "Buscar:",
+                "zeroRecords": "Ningun producto encontrado ..."
+
+            },
+            "columnDefs": [{// set default column settings
+                    'orderable': true,
+                    'targets': [0]
+                }, {
+                    "searchable": true,
+                    "targets": [0]
+                }],
+            "order": [
+                [0, "DESC"]
+            ]
+        });
+        var tableWrapper = $('#products_table_wrapper');
+        tableWrapper.find('.dataTables_length select').select2();
+    };
+
+</script>
